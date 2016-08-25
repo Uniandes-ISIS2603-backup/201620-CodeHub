b3afa4ada8dadb5d147c88936a17e38e62673d7c
czambrano10 <c.zambrano10@uniandes.edu.co>
2016-08-25 12:18:03 -0500

    agregado un ejemplo del gitbook
    :tada: :tada: porque si :tada:  :tada:

--- src/main/webapp/src/modules/Equipos/equipos.mod.js
+++ src/main/webapp/src/modules/Equipos/equipos.mod.js
@@ -5,8 +5,19 @@
  */
 
 var mod = ng.module("equiposModule", ["ui-router"]);
+
+mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    var basePath = 'src/modules/equipos/';
+    $urlRouterProvider.otherwise("/business");
+    $stateProvider
+            .state('equipo', {
+                        url: "/equipo",
+                        templateUrl: basePath+"equipo.html"
+                    });
+    }]);
+
 function nada (entero){
-    if(entero==1 || entero==0){return 1;}
+    if(entero===1 || entero===0){return 1;}
     if (entero>1){
         return nada(entero-1)+nada(entero-2);
     }
