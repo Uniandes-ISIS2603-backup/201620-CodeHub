befa2606ce6970b15be4bcfb56954d1c215b9e7d
d.cagua10 <d.cagua10@ISIS2603-96.sis.virtual.uniandes.edu.co>
2016-08-29 20:39:58 -0500

    Arreglos Modulo Admin

--- src/main/webapp/src/modules/Administradores/admin.mod.js
+++ src/main/webapp/src/modules/Administradores/admin.mod.js
@@ -4,5 +4,44 @@
  * and open the template in the editor.
  */
 
+(function (ng) {
+    var mod = ng.module("adminModule", ["ngMessages"]);
+    mod.constant("adminContext", "api/admin");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/admin/';
+            $urlRouterProvider.otherwise("/adminList");
+     
+            $stateProvider.state('adminList', {
+                url: '/cities',
+                views: {
+                    'mainView': {
+                        controller: 'adminCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'admin.list.html'
+                    }
+                }
+            }).state('adminCreate', {
+                url: '/admins/create',
+                views: {
+                    'mainView': {
+                        controller: 'adminCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'cADMIN.create.html'
+                    }
+                }
 
-var mod = ng.module("adminModule", ["ui-router"]);
+            }).state('adminEdit', {
+                url: '/admin/:adminId',
+                param: {
+                    cityId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'adminCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'admin.create.html'
+                    }
+                }
+            });
+        }]);
+})(window.angular);
