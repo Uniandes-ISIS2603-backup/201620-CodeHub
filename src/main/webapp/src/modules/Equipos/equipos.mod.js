5c468ec65865699a8c3ae4f5b07a7f7f732ff507
czambrano10 <c.zambrano10@uniandes.edu.co>
2016-08-30 08:37:27 -0500

    arreglos html
    :rage4: pero aun no consigo que funcione!

--- src/main/webapp/src/modules/Equipos/equipos.mod.js
+++ src/main/webapp/src/modules/Equipos/equipos.mod.js
@@ -3,22 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+(function (ng) {
+    var mod = ng.module("equiposModule", ["ui-router"]);
+    mod.constant("equiposContext", "api/equipos");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/equipos/';
+            $urlRouterProvider.otherwise("/equiposList");
+     
+            $stateProvider.state('equiposList', {
+                url: '/equipos',
+                views: {
+                    'mainView': {
+                        controller: 'equiposCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'equipos.list.html'
+                    }
+                }
+            }).state('equiposCreate', {
+                url: '/equipos/create',
+                views: {
+                    'mainView': {
+                        controller: 'equiposCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'equipos.create.html'
+                    }
+                }
 
-var mod = ng.module("equiposModule", ["ui-router"]);
-
-mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-    var basePath = 'src/modules/equipos/';
-    $urlRouterProvider.otherwise("/business");
-    $stateProvider
-            .state('equipo', {
-                        url: "/equipo",
-                        templateUrl: basePath+"equipo.html"
-                    });
-    }]);
-
-function nada (entero){
-    if(entero===1 || entero===0){return 1;}
-    if (entero>1){
-        return nada(entero-1)+nada(entero-2);
-    }
-}
+            }).state('equipoEdit', {
+                url: '/equipos/:equipoCodigo',
+                param: {
+                    equipoCodigo: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'equiposCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'equipos.create.html'
+                    }
+                }
+            });
+        }]);
+})(window.angular);
