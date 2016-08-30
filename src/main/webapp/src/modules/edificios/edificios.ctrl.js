43855df03885df3e4fad8ca8efb86be3ac1b8ab1
Sebastian Martinez <s.martinez15@uniandes.edu.co>
2016-08-30 13:02:54 -0500

    implementacion DELETE edificios
    Pueden copiarse de la implementacion en el edificios.ctrl, el metodo de
    removeRecord(id) :8ball:

--- src/main/webapp/src/modules/edificios/edificios.ctrl.js
+++ src/main/webapp/src/modules/edificios/edificios.ctrl.js
@@ -63,6 +63,22 @@
                         }, responseError);
                 };
             };
+            
+            this.deleteRecord = function (id) {
+                currentRecord = $scope.currentRecord;
+                if(id!=null)
+                {            
+                    // ejecuta delete en el recurso REST
+                    return $http.delete(context + "/" + id,currentRecord)
+                        .then(function () {
+                            $scope.records = {};
+                            $http.get(context).then(function(response){
+                                $scope.records = response.data;    
+                            }, responseError);
+                            $state.go('edificiosList');
+                        }, responseError); 
+                }
+                };
 
 
 
