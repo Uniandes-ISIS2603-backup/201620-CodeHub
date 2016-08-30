b1123370d27ddc8bf91797f1d549438c2fbd85bd
Sebastian Martinez <s.martinez15@uniandes.edu.co>
2016-08-29 21:59:57 -0500

    html edificios

--- src/main/webapp/src/modules/edificios/edificios.mod.js
+++ src/main/webapp/src/modules/edificios/edificios.mod.js
@@ -3,5 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+(function (ng) {
+    var mod = ng.module("edificiosModule", ["ui-router"]);
+    mod.constant("edificiosContext", "api/edificios");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/edificios/';
+            $urlRouterProvider.otherwise("/edificiosList");
+     
+            $stateProvider.state('edificiosList', {
+                url: '/edificios',
+                views: {
+                    'mainView': {
+                        controller: 'edificiosCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'edificios.list.html'
+                    }
+                }
+            }).state('edificioCreate', {
+                url: '/edificios/create',
+                views: {
+                    'mainView': {
+                        controller: 'edificiosCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'edificios.create.html'
+                    }
+                }
 
-var mod = ng.module("edificiosModule", ["ui-router"]);
+            }).state('edificioEdit', {
+                url: '/edificios/:edificioId',
+                param: {
+                    edificioId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'edificiosCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'edificios.create.html'
+                    }
+                }
+            });
+        }]);
+})(window.angular);
+
