5905f31d1df64d85f9dc7ea8f728dd974a2d19d3
czambrano10 <c.zambrano10@uniandes.edu.co>
2016-08-30 07:30:36 -0500

    modificaciones js equipos

--- src/main/webapp/src/modules/Equipos/equipos.ctrl.js
+++ src/main/webapp/src/modules/Equipos/equipos.ctrl.js
@@ -3,5 +3,67 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var mod = ng.module("equiposModule");
+
+    mod.controller("equiposCtrl", ['$scope', '$state', '$stateParams', '$http', 'citiesContext'])= function ($scope, $state, $stateParams, $http, context) {
+
+            // inicialmente el listado de equipos está vacio
+            $scope.records = {};
+            // carga los equipos
+            $http.get(context).then(function(response){
+                $scope.records = response.data;    
+            }, responseError);
+
+            // el controlador recibió un cityId ??
+            // revisa los parámetros (ver el :cityId en la definición de la ruta)
+            if ($stateParams.cityId !== null && $stateParams.cityId !== undefined) {
+
+                // toma el id del parámetro
+                id = $stateParams.cityId;
+                // obtiene el dato del recurso REST
+                $http.get(context + "/" + id)
+                    .then(function (response) {
+                        // $http.get es una promesa
+                        // cuando llegue el dato, actualice currentRecord
+                        $scope.currentRecord = response.data;
+                    }, responseError);
+
+            // el controlador no recibió un cityId
+            } else
+            {
+                // el registro actual debe estar vacio
+                $scope.currentRecord = {
+                    id: undefined /*Tipo Long. El valor se asigna en el backend*/,
+                    name: '' /*Tipo String*/,
+                };
+
+                $scope.alerts = [];
+            }
 
 
+            this.saveRecord = function (id) {
+                currentRecord = $scope.currentRecord;
+
+                // si el id es null, es un registro nuevo, entonces lo crea
+                if (id == null) {
+
+                    // ejecuta POST en el recurso REST
+                    return $http.post(context, currentRecord)
+                        .then(function () {
+                            // $http.post es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('citiesList');
+                        }, responseError);
+
+                // si el id no es null, es un registro existente entonces lo actualiza
+                } else {
+
+                    // ejecuta PUT en el recurso REST
+                    return $http.put(context + "/" + currentRecord.id, currentRecord)
+                        .then(function () {
+                            // $http.put es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('citiesList');
+                        }, responseError);
+                };
+            };
