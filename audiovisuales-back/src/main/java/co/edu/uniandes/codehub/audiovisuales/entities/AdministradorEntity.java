eb0172bccb2d85dab8094b8c0fb9188c69dfcaab
dcagua10 <d.cagua10@ISIS2603-96.sis.virtual.uniandes.edu.co>
2016-10-06 12:02:44 -0500

    AdministradorEntity

---
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/AdministradorEntity.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.codehub.audiovisuales.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author d.cagua10
+ */
+@Entity
+public class AdministradorEntity extends BaseEntity implements Serializable
+{
+    
+}
+ 
