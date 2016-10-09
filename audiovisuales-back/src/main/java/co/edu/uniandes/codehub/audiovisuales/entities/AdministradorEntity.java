2b51dee22d8f7216b9f685ad720a407a7a5a8506
danie <d.cagua10@uniandes.edu.co>
2016-10-09 13:57:46 -0500

    Admin update

--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/AdministradorEntity.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/AdministradorEntity.java
@@ -8,6 +8,10 @@ package co.edu.uniandes.codehub.audiovisuales.entities;
 import java.io.Serializable;
 import javax.persistence.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+
 /**
  *
  * @author d.cagua10
@@ -15,6 +19,85 @@ import javax.persistence.Entity;
 @Entity
 public class AdministradorEntity extends BaseEntity implements Serializable
 {
-    //...
+    private Long id;
+    private String nombre;
+    private String correo;
+    
+    private EdificioEntity edificio;
+    
+     /**
+     * Obtiene el atributo id.
+     *
+     * @return atributo id.
+     *
+     */
+    public Long getId()
+    {
+        return id;
+    }
+    
+     /**
+     * Obtiene el atributo nombre.
+     *
+     * @return atributo nombre.
+     *
+     */
+    public String getNombre()
+    {
+        return nombre;
+    }
+    
+     /**
+     * Establece el valor del atributo nombre.
+     *
+     * @param nombre nuevo valor del atributo
+     *
+     */
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+    
+      /**
+     * Obtiene el atributo correo.
+     *
+     * @return atributo correo.
+     *
+     */
+    public String getCorreo()
+    {
+        return correo;
+    }
+    
+     /**
+     * Establece el valor del atributo correo.
+     *
+     * @param correo nuevo valor del atributo
+     *
+     */
+    public void setCorreo(String correo) 
+    {
+        this.correo = correo;
+    }
+    
+     /**
+     * Obtiene la colección de edificios.
+     * @return colección edificios. 
+     */
+    public EdificioEntity getEdificio() 
+    {
+        return edificio;
+    }
+
+    /**
+     * Establece el valor de edificio.
+     * @param edificio nuevo valor. 
+     */
+    public void setEdificio(EdificioEntity edificio)
+    {
+        this.edificio = edificio;
+    }
+    
+    
 }
  
--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/AdministradorPersistence.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/AdministradorPersistence.java
@@ -6,15 +6,20 @@
 package co.edu.uniandes.codehub.audiovisuales.persistence;
 
 import co.edu.uniandes.codehub.audiovisuales.entities.AdministradorEntity;
+import javax.ejb.Stateless;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 
 /**
  *
  * @author d.cagua10
  */
+@Stateless
 public class AdministradorPersistence 
 {
     
@@ -23,9 +28,46 @@ public class AdministradorPersistence
     @PersistenceContext(unitName = "CodehubPU")
     protected EntityManager em;  
     
-     public AdministradorEntity find(Long id) {
-        LOGGER.log(Level.INFO, "Consultando company con id={0}", id);
+     public AdministradorEntity find(Long id) 
+     {
+        LOGGER.log(Level.INFO, "Consultando administrador con id={0}", id);
         return em.find(AdministradorEntity.class, id);
+     }
+     
+      public AdministradorEntity findByName(String nombre) 
+      {
+        LOGGER.log(Level.INFO, "Consultando administrador con nombre= ", nombre);
+        TypedQuery<AdministradorEntity> q = em.createQuery("select u from AdministradorEntity u where u.nombre = :nombre", AdministradorEntity.class);
+        q = q.setParameter("nombre", nombre);
+        return q.getSingleResult();
+       }
+     
+     public List<AdministradorEntity> findAll() 
+     {
+        LOGGER.info("Consultando todos los administradores");
+        Query q = em.createQuery("select u from AdministradorEntity u");
+        return q.getResultList();
+    }
+
+    public AdministradorEntity create(AdministradorEntity entity) 
+    {
+        LOGGER.info("Creando un administrador nuevo");
+        em.persist(entity);
+        LOGGER.info("Administrador creado");
+        return entity;
+    }
+
+    public AdministradorEntity update(AdministradorEntity entity) 
+    {
+        LOGGER.log(Level.INFO, "Actualizando administrador con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    public void delete(Long id) 
+    {
+        LOGGER.log(Level.INFO, "Borrando administrador con id={0}", id);
+        AdministradorEntity entity = em.find(AdministradorEntity.class, id);
+        em.remove(entity);
     }
     
 }
