54d4f6fc826429f2f9fef1d00c51e54cf00b8d8b
osabogal10 <o.sabogal10@uniandes.edu.co>
2016-10-10 21:21:21 -0500

    R

--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/ReservaEntity.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/ReservaEntity.java
@@ -7,10 +7,100 @@
 package co.edu.uniandes.codehub.audiovisuales.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 
 @Entity
-
 public class ReservaEntity extends BaseEntity implements Serializable{
 
+    public final static int EN_REVISION = 1;
+    public final static int ACTIVA = 2;
+    public final static int INACTIVA = 3;
+    public final static int CANCELADA = 4;
+    public final static int RECHAZADA = 5;
+    
+    private int estado; //1-Creada sin aprobar, 2-Activa, 3-Inactiva, 4-Cancelada, 5-Rechazada
+    private Date fechaInicial;
+    private Date fechaFinal;
+    private Double calificacion;
+    private Boolean generoSancion;
+    private Long edificioId;
+    private Long id;
+    private Long idEquipo;
+    private Long idUsuario;
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaInicial() {
+        return fechaInicial;
+    }
+
+    public void setFechaInicial(Date fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public Double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Boolean getGeneroSancion() {
+        return generoSancion;
+    }
+
+    public void setGeneroSancion(Boolean generoSancion) {
+        this.generoSancion = generoSancion;
+    }
+
+    public Long getEdificioId() {
+        return edificioId;
+    }
+
+    public void setEdificioId(Long edificioId) {
+        this.edificioId = edificioId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(Long idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+    
+    
 }
--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/ReservaPersistence.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/ReservaPersistence.java
@@ -6,17 +6,19 @@
 
 package co.edu.uniandes.codehub.audiovisuales.persistence;
 
-import co.edu.uniandes.codehub.audiovisuales.entities.EdificioEntity;
 import co.edu.uniandes.codehub.audiovisuales.entities.ReservaEntity;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
+ * @author o.sabogal10
  */
 @Stateless
 public class ReservaPersistence 
@@ -30,5 +32,41 @@ public class ReservaPersistence
      public ReservaEntity find(Long id) {
         LOGGER.log(Level.INFO, "Consultando company con id={0}", id);
         return em.find(ReservaEntity.class, id);
+    }
+     
+     public ReservaEntity findByName(String nombre) 
+      {
+        LOGGER.log(Level.INFO, "Consultando Reserva con nombre= ", nombre);
+        TypedQuery<ReservaEntity> q = em.createQuery("select u from ReservaEntity u where u.nombre = :nombre", ReservaEntity.class);
+        q = q.setParameter("nombre", nombre);
+        return q.getSingleResult();
+       }
+     
+     public List<ReservaEntity> findAll() 
+     {
+        LOGGER.info("Consultando todos los Reservaes");
+        Query q = em.createQuery("select u from ReservaEntity u");
+        return q.getResultList();
+    }
+
+    public ReservaEntity create(ReservaEntity entity) 
+    {
+        LOGGER.info("Creando un Reserva nuevo");
+        em.persist(entity);
+        LOGGER.info("Reserva creado");
+        return entity;
+    }
+
+    public ReservaEntity update(ReservaEntity entity) 
+    {
+        LOGGER.log(Level.INFO, "Actualizando Reserva con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    public void delete(Long id) 
+    {
+        LOGGER.log(Level.INFO, "Borrando Reserva con id={0}", id);
+        ReservaEntity entity = em.find(ReservaEntity.class, id);
+        em.remove(entity);
     }
 }
