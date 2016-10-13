a52aa18c4af921d1ab0b2225f92524e4b769e054
osabogal10 <o.sabogal10@uniandes.edu.co>
2016-10-13 15:53:40 -0500

    ReservaPersistenceTest

--- audiovisuales-back/src/test/java/co/edu/uniandes/codehub/audiovisuales/test/persistence/ReservaPersistenceTest.java
+++ audiovisuales-back/src/test/java/co/edu/uniandes/codehub/audiovisuales/test/persistence/ReservaPersistenceTest.java
@@ -5,18 +5,57 @@
  */
 package co.edu.uniandes.codehub.audiovisuales.test.persistence;
 
+import co.edu.uniandes.codehub.audiovisuales.entities.ReservaEntity;
+import co.edu.uniandes.codehub.audiovisuales.persistence.ReservaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
+@RunWith(Arquillian.class)
 public class ReservaPersistenceTest {
+    
+    @Deployment
+    public static JavaArchive createDeployment()
+    {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ReservaEntity.class.getPackage())
+                .addPackage(ReservaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+                
+    }
+    
+    @Inject
+    private ReservaPersistence reservaPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+    private List<ReservaEntity> data = new ArrayList<ReservaEntity>();
+    
     
     public ReservaPersistenceTest() {
     }
@@ -31,6 +70,23 @@ public class ReservaPersistenceTest {
     
     @Before
     public void setUp() {
+        try{
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        }
+        catch (Exception e) {
+            try
+            {
+                utx.rollback();
+            }
+            catch (Exception e1){
+                e1.printStackTrace();
+                fail("configuracion data base fail");
+            }
+        }
     }
     
     @After
@@ -43,6 +99,21 @@ public class ReservaPersistenceTest {
     @Test
     public void testFind() throws Exception {
         
+    }
+
+    private void clearData() {
+        em.createQuery("delete from DepartmentEntity").executeUpdate();
+        em.createQuery("delete from CompanyEntity").executeUpdate();
+    }
+
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i=0;i<3;i++)
+        {
+            ReservaEntity entity = factory.manufacturePojo(ReservaEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
     }
     
 }
