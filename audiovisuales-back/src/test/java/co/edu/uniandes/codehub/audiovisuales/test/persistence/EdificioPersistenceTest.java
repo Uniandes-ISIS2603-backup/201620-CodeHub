63e22e6c1ab50c32695786c42b49ae93a39e5a96
Sebastian Martinez <s.martinez15@uniandes.edu.co>
2016-10-14 12:21:46 -0500

    Añadi algunas lineas a EdificioPersistenceTest

--- audiovisuales-back/src/test/java/co/edu/uniandes/codehub/audiovisuales/test/persistence/EdificioPersistenceTest.java
+++ audiovisuales-back/src/test/java/co/edu/uniandes/codehub/audiovisuales/test/persistence/EdificioPersistenceTest.java
@@ -5,41 +5,82 @@
  */
 package co.edu.uniandes.codehub.audiovisuales.test.persistence;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import co.edu.uniandes.codehub.audiovisuales.entities.EdificioEntity;
+import co.edu.uniandes.codehub.audiovisuales.persistence.EdificioPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
  * @author s.martinez15
  */
+@RunWith(Arquillian.class)
 public class EdificioPersistenceTest {
     
-    public EdificioPersistenceTest() {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(EdificioEntity.class.getPackage())
+                .addPackage(EdificioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
+
+    @Inject
+    private EdificioPersistence companyPersistence;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    private List<EdificioEntity> data = new ArrayList<>();
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
     
     @Before
     public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
     }
     
-    @After
-    public void tearDown() {
+    private void clearData() {
+        em.createQuery("delete from AdminEntity").executeUpdate();
+        em.createQuery("delete from EdificioEntity").executeUpdate();
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            EdificioEntity entity = factory.manufacturePojo(EdificioEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
 }
