3dda7722c6f7189b2a95e9cc4964c8bd37f69ff1
Sebastian Martinez <s.martinez15@uniandes.edu.co>
2016-10-14 12:09:53 -0500

    Entity Edificio creado

--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/EdificioEntity.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/EdificioEntity.java
@@ -6,7 +6,12 @@
 package co.edu.uniandes.codehub.audiovisuales.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import uk.co.jemos.podam.common.PodamExclude;
 
 /**
  * 
@@ -15,5 +20,61 @@ import javax.persistence.Entity;
 @Entity
 public class EdificioEntity extends BaseEntity implements Serializable
 {
-    
+    /**
+     * Abreviatura del edificio
+     */
+   private String bloque; 
+   
+   //-----------
+    // Metodos
+    //-----------
+   
+   /**
+     * Administrador del edificio
+     */
+   @PodamExclude
+   @OneToOne
+   private AdministradorEntity admin; 
+   
+   /**
+     * Equipos del edificio
+     */
+   @PodamExclude
+   @OneToMany
+   private List<EquipoEntity> equipos = new ArrayList<>(); 
+   
+   
+    //-----------
+    // Metodos
+    //-----------
+   
+   public void setBloque(String b)
+   {
+       this.bloque = b;
+   }
+   
+   public String getBloque()
+   {
+       return this.bloque;
+   }
+   
+   public void setAdmin(AdministradorEntity a)
+   {
+       this.admin = a;
+   }
+   
+   public AdministradorEntity getAdmin()
+   {
+       return admin;
+   }
+   
+   public void setEquipos(ArrayList<EquipoEntity> e)
+   {
+       this.equipos = e;
+   }
+   
+   public List<EquipoEntity> getEquipos()
+   {
+       return equipos;
+   }
 }
--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/EdificioPersistence.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/EdificioPersistence.java
@@ -6,11 +6,14 @@
 package co.edu.uniandes.codehub.audiovisuales.persistence;
 
 import co.edu.uniandes.codehub.audiovisuales.entities.EdificioEntity;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -26,7 +29,39 @@ public class EdificioPersistence
     protected EntityManager em;  
     
      public EdificioEntity find(Long id) {
-        LOGGER.log(Level.INFO, "Consultando company con id={0}", id);
+        LOGGER.log(Level.INFO, "Consultando edificio con id={0}", id);
         return em.find(EdificioEntity.class, id);
+     }
+
+    public EdificioEntity findByName(String name) {
+        LOGGER.log(Level.INFO, "Consultando company con name = {0}", name);
+        TypedQuery<EdificioEntity> q
+                = em.createQuery("select e from EdificioEntity e where e.name = :name", EdificioEntity.class);
+        q = q.setParameter("name", name); 
+        return q.getSingleResult();
+    }
+
+    public List<EdificioEntity> findAll() {
+        LOGGER.info("Consultando todos los edificios");
+        Query q = em.createQuery("select e from EdificioEntity e");
+        return q.getResultList();
+    }
+
+    public EdificioEntity create(EdificioEntity entity) {
+        LOGGER.info("Creando un edificio");
+        em.persist(entity);
+        LOGGER.info("Edificio creado");
+        return entity;
+    }
+
+    public EdificioEntity update(EdificioEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando edificio con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando edificio con id={0}", id);
+        EdificioEntity entity = em.find(EdificioEntity.class, id);
+        em.remove(entity);
     }
 }
