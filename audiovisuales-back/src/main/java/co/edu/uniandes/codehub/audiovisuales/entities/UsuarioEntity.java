68afcb1db8ce9272154c607afd8bb3ba55052115
lnbello10 <ln.bello10@uniandes.edu.co>
2016-10-14 11:26:42 -0500

    entity

--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/UsuarioEntity.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/UsuarioEntity.java
@@ -18,23 +18,9 @@ public class UsuarioEntity extends BaseEntity implements Serializable {
     public final static int ESTUDIANTE = 1;
     public final static int PROFESOR = 2;
     
-    private String name;
-    private Long id;
     private Boolean tieneSancion;
     private int tipo;
     
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public Long getId(){
-        return this.id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
     public Boolean getTieneSancion(){
         return this.tieneSancion;
     }
