d76f40e9fcc7cfc2f1bfce8168b5b49831c65d45
Sebastian Martinez <s.martinez15@uniandes.edu.co>
2016-10-18 13:07:16 -0500

    Commit 3: Summer is coming.

--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/AdministradorEntity.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/entities/AdministradorEntity.java
@@ -8,8 +8,6 @@ package co.edu.uniandes.codehub.audiovisuales.entities;
 import java.io.Serializable;
 import javax.persistence.Entity;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.FetchType;
 import javax.persistence.OneToOne;
--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/UsuarioPersistence.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/persistence/UsuarioPersistence.java
@@ -14,8 +14,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-import static javax.ws.rs.client.Entity.entity;
 
 /**
  *
