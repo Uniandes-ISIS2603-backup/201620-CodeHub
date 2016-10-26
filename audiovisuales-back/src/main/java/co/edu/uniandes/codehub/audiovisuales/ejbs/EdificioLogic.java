94255b6d12321f28ccd91a6c5c69ed84dceded17
Sebastian Martinez <s.martinez15@uniandes.edu.co>
2016-10-26 12:18:15 -0500

    Cree el paquete de test de la logica e implemente el EdificioLogicTest.

--- audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/ejbs/EdificioLogic.java
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/ejbs/EdificioLogic.java
@@ -5,6 +5,7 @@
  */
 package co.edu.uniandes.codehub.audiovisuales.ejbs;
 
+import co.edu.uniandes.codehub.audiovisuales.api.IEdificioLogic;
 import co.edu.uniandes.codehub.audiovisuales.entities.EdificioEntity;
 import co.edu.uniandes.codehub.audiovisuales.exceptions.AudiovisualesLogicException;
 import co.edu.uniandes.codehub.audiovisuales.persistence.EdificioPersistence;
@@ -17,50 +18,60 @@ import javax.inject.Inject;
  * @author s.martinez15
  */
 @Stateless
-public class EdificioLogic {
+public class EdificioLogic implements IEdificioLogic {
     
     @Inject
     private EdificioPersistence persistence;
     
+    @Override
     public List<EdificioEntity> getEdificios() {
         return persistence.findAll();
     }
     
+    @Override
     public EdificioEntity getEdificio(Long id)
     {
         return persistence.find(id);
     }
     
+    @Override
     public EdificioEntity getEdificioByName(String name) {
         return persistence.findByName(name);
     }
     
+    @Override
     public EdificioEntity getEdificioByBloque(String bloque) {
         return persistence.findByBloque(bloque);
     }
-       
+    
+    @Override
     public EdificioEntity getEdificioByAdmin(Long adminId)
     {
         return persistence.findByAdmin(adminId);
     }
     
+    @Override
     public EdificioEntity createEdificio(EdificioEntity entity) throws AudiovisualesLogicException {
-        EdificioEntity existe = getEdificioByName(entity.getName());
-        if (existe != null) 
+        EdificioEntity existeName = getEdificioByName(entity.getName());
+        if (existeName != null) 
         {
             throw new AudiovisualesLogicException("Ya existe un edificio con ese nombre");
         } 
-        else
+        EdificioEntity existeBloque = getEdificioByBloque(entity.getBloque());
+        if (existeBloque != null) 
         {
-            persistence.create(entity);
-        }
+            throw new AudiovisualesLogicException("Ya existe un edificio con ese bloque");
+        } 
+        persistence.create(entity);
         return entity;
     }
     
+    @Override
     public EdificioEntity updateEdificio(EdificioEntity entity) {
         return persistence.update(entity);
     }
 
+    @Override
     public void deleteEdificio(Long id) {
         persistence.delete(id);
     }
---
+++ audiovisuales-back/src/test/java/co/edu/uniandes/codehub/audiovisuales/test/logic/EdificioLogicTest.java
@@ -0,0 +1,220 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.codehub.audiovisuales.test.logic;
+
+import co.edu.uniandes.codehub.audiovisuales.api.IEdificioLogic;
+import co.edu.uniandes.codehub.audiovisuales.ejbs.EdificioLogic;
+import co.edu.uniandes.codehub.audiovisuales.entities.EdificioEntity;
+import co.edu.uniandes.codehub.audiovisuales.exceptions.AudiovisualesLogicException;
+import co.edu.uniandes.codehub.audiovisuales.persistence.EdificioPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author s.martinez15
+ */
+@RunWith(Arquillian.class)
+public class EdificioLogicTest {
+    
+    private PodamFactory factory = new PodamFactoryImpl();
+    
+    @Inject
+    private IEdificioLogic logic;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    private UserTransaction utx;
+    
+    private List<EdificioEntity> data = new ArrayList<EdificioEntity>();
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(EdificioEntity.class.getPackage())
+                .addPackage(EdificioLogic.class.getPackage())
+                .addPackage(IEdificioLogic.class.getPackage())
+                .addPackage(EdificioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+     private void clearData() {
+        em.createQuery("delete from EdificioEntity").executeUpdate();
+    }
+
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 5; i++) {            
+            EdificioEntity entity = factory.manufacturePojo(EdificioEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+    
+    /**
+     * Prueba para crear un edificio con un nombre y bloque que no existe
+     */
+    @Test
+    public void createEdificioSuccesTest() throws AudiovisualesLogicException {
+        EdificioEntity newEntity = factory.manufacturePojo(EdificioEntity.class);
+
+        EdificioEntity result =logic.createEdificio(newEntity);
+        Assert.assertNotNull(result);
+
+        EdificioEntity entity = em.find(EdificioEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+        Assert.assertEquals(newEntity.getBloque(), entity.getBloque());
+        Assert.assertEquals(newEntity.getId(), entity.getId());
+    }
+
+    /**
+     * Prueba para crear un edificio con un nombre que ya existe
+     */
+    @Test(expected = AudiovisualesLogicException.class)
+    public void createEdificioFailTest1() throws Exception {
+        EdificioEntity newEntity = factory.manufacturePojo(EdificioEntity.class);
+        newEntity.setName(data.get(0).getName());
+        EdificioEntity resultEntity = logic.createEdificio(newEntity);
+        Assert.assertNull(resultEntity);
+    }
+    
+        /**
+     * Prueba para crear un edificio con un bloque que ya existe
+     */
+    @Test(expected = AudiovisualesLogicException.class)
+    public void createEdificioFailTest2() throws Exception {
+        EdificioEntity newEntity = factory.manufacturePojo(EdificioEntity.class);
+        newEntity.setBloque(data.get(0).getBloque());
+        EdificioEntity resultEntity = logic.createEdificio(newEntity);
+        Assert.assertNull(resultEntity);
+    }
+    
+    /**
+     * Prueba para obtener la lista de edificios
+     */
+    @Test
+    public void getEdificiosTest() {
+        List<EdificioEntity> list = logic.getEdificios();
+        Assert.assertEquals(data.size(), list.size());
+        for (EdificioEntity entity : list) 
+        {
+            boolean found = false;
+            for (EdificioEntity dataEntity : data) {
+                if (entity.getId().equals(dataEntity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    /**
+     * Prueba para obtener un edificio.
+     */
+    @Test
+    public void getEdificioTest() {
+        EdificioEntity entity = data.get(0);
+        EdificioEntity resultEntity = logic.getEdificio(entity.getId());
+        Assert.assertNotNull(resultEntity);
+        Assert.assertEquals(entity.getName(), resultEntity.getName());
+        Assert.assertEquals(entity.getBloque(), resultEntity.getBloque());
+        Assert.assertEquals(entity.getId(), resultEntity.getId());
+    }
+    
+    /**
+     * Prueba para obtener un edificio por nombre.
+     */
+    @Test
+    public void getEdificioByNameTest() {
+        EdificioEntity entity = data.get(1);
+        EdificioEntity resultEntity = logic.getEdificioByName(entity.getName());
+        Assert.assertNotNull(resultEntity);
+        Assert.assertEquals(entity.getName(), resultEntity.getName());
+        Assert.assertEquals(entity.getBloque(), resultEntity.getBloque());
+        Assert.assertEquals(entity.getId(), resultEntity.getId());
+    }
+    
+    /**
+     * Prueba para obtener un edificio por bloque.
+     */
+    @Test
+    public void getEdificioByBloqueTest() {
+        EdificioEntity entity = data.get(1);
+        EdificioEntity resultEntity = logic.getEdificioByBloque(entity.getBloque());
+        Assert.assertNotNull(resultEntity);
+        Assert.assertEquals(entity.getName(), resultEntity.getName());
+        Assert.assertEquals(entity.getBloque(), resultEntity.getBloque());
+        Assert.assertEquals(entity.getId(), resultEntity.getId());
+    }
+    
+    /**
+     * Prueba para actualizar un edificio.
+     */
+    @Test
+    public void updateEdificioTest() {
+        EdificioEntity entity = data.get(0);
+        EdificioEntity updatedEntity = factory.manufacturePojo(EdificioEntity.class);
+
+        updatedEntity.setId(entity.getId());
+
+        logic.updateEdificio(updatedEntity);
+
+        EdificioEntity resultEntity = em.find(EdificioEntity.class, entity.getId());
+
+        Assert.assertEquals(updatedEntity.getName(), resultEntity.getName());
+        Assert.assertEquals(updatedEntity.getBloque(), resultEntity.getBloque());
+        Assert.assertEquals(updatedEntity.getId(), resultEntity.getId());
+    }
+    
+    /**
+     * Prueba para eliminar un edificio.
+     */
+    @Test
+    public void deleteEdificioTest() {
+        EdificioEntity entity = data.get(1);
+        logic.deleteEdificio(entity.getId());
+        EdificioEntity resultEntity = em.find(EdificioEntity.class, entity.getId());
+        Assert.assertNull(resultEntity);
+    }
+}
