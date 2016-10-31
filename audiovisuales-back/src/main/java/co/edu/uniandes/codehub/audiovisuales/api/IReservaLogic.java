c189da9c7ed87e52c728afcfaace2c5d277ce7fc
osabogal10 <o.sabogal10@uniandes.edu.co>
2016-10-31 00:54:45 -0500

    ReservaLogic

---
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/api/IReservaLogic.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.codehub.audiovisuales.api;
+
+import co.edu.uniandes.codehub.audiovisuales.entities.ReservaEntity;
+import java.util.List;
+
+/**
+ *
+ * @author o.sabogal10
+ */
+public interface IReservaLogic {
+    
+    public List<ReservaEntity> getReservas();
+    
+    public ReservaEntity getReserva(Long id);
+    
+    public ReservaEntity getReservaByUsuario(String name);
+    
+    public ReservaEntity createReserva(ReservaEntity entity);
+    
+    public ReservaEntity updateReserva(ReservaEntity entity);
+    
+    public void deleteReserva(Long id);
+    
+}
---
+++ audiovisuales-back/src/main/java/co/edu/uniandes/codehub/audiovisuales/ejbs/ReservaLogic.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.codehub.audiovisuales.ejbs;
+
+import co.edu.uniandes.codehub.audiovisuales.api.IReservaLogic;
+import co.edu.uniandes.codehub.audiovisuales.entities.ReservaEntity;
+import co.edu.uniandes.codehub.audiovisuales.persistence.ReservaPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ * 
+ * @author o.sabogal10
+ */
+@Stateless
+public class ReservaLogic implements IReservaLogic{
+    
+    @Inject
+    private ReservaPersistence persistence;
+
+    @Override
+    public List<ReservaEntity> getReservas() {
+        return persistence.findAll();  }
+
+    @Override
+    public ReservaEntity getReserva(Long id) {
+       return persistence.find(id);
+    }
+
+    @Override
+    public ReservaEntity getReservaByUsuario(String name) {
+       return persistence.findByName(name);
+    }
+
+    @Override
+    public ReservaEntity createReserva(ReservaEntity entity) {
+        return persistence.create(entity);
+    }
+
+    @Override
+    public ReservaEntity updateReserva(ReservaEntity entity) {
+        return persistence.update(entity);
+    }
+
+    @Override
+    public void deleteReserva(Long id) {
+        persistence.delete(id);
+    }
+
+
+}
