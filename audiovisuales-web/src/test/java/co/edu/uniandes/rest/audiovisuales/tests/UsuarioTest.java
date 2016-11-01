9a4003a91fbbb33974e313b7645ecd07bd32edd9
czambrano10 <c.zambrano10@uniandes.edu.co>
2016-11-01 10:12:41 -0500

    Revert "En busqueda de la esfera azul: despedida al caos :large_blue_circle:"
    This reverts commit fbafed71f49d87176346db44b3d6431449f3bf2e.

---
+++ audiovisuales-web/src/test/java/co/edu/uniandes/rest/audiovisuales/tests/UsuarioTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.rest.audiovisuales.tests;
+
+/**
+ *
+ * @author c.zambrano10
+ */
+public class UsuarioTest {
+    /**
+     *  voy a subir esto y luego lo voy a borrar confiando en que así 
+     *  se solucione ese commit fantasma.
+     */
+}
